74d661f3702cb923f1c732035dd8ab429f53fd6f
Artem Oliinyk <azure.email@gmail.com>
2022-09-11 20:41:56 +0200

    ver. 2 with visited

--- task1/go/task1.go
+++ task1/go/task1.go
@@ -1,14 +1,16 @@
 package task1
 
 func twoSum(nums []int, target int) []int {
-	for i1, n1 := range nums {
-		for i2, n2 := range nums[i1+1:] {
-			
-			if n1+n2 == target {
-				return []int{i1, i2+i1+1}
-			}
+	// v2
+	seen := make(map[int]int)
+
+	for i, v := range nums {
+		if seenIdx, exist := seen[target-v]; exist {
+			return []int{i, seenIdx}
 		}
+
+		seen[v] = i
 	}
 
-	return []int{-1, -1}
+	return []int{0, 0}
 }
