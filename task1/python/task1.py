d96ff3af34f576a913227c751ac240fe8ede78ea
Artem Oliinyk <azure.email@gmail.com>
2022-09-12 20:11:15 +0200

    python version

--- task1/python/task1.py
+++ task1/python/task1.py
@@ -2,7 +2,13 @@
 
 from typing import List
 
-
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        visited = {}
+
+        for i, v in enumerate(nums):
+            if (target - v) in visited:
+                return [i, visited[target - v]]
+            visited[v] = i
+        
         return [-1, -1]
