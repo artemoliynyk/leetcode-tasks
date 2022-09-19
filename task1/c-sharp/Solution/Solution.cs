89602a823a24bf064ace6de68f4d413ba5c49ba2
Artem Oliinyk <azure.email@gmail.com>
2022-09-19 21:46:55 +0200

    implementation with tests

--- task1/c-sharp/Solution/Solution.cs
+++ task1/c-sharp/Solution/Solution.cs
@@ -2,14 +2,20 @@ namespace Task1
 {
     public class Solution
     {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Test123");
-            Console.WriteLine("OK");
-        }
-
         public int[] TwoSum(int[] nums, int target)
         {
+            var visited = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var diff = target - nums[i];
+                if (visited.ContainsKey(diff))
+                {
+                    return new int[] { i, visited[diff] };
+                }
+                visited[nums[i]] = i;
+            }
+
             return new int[] { -1, -1 };
         }
     }
